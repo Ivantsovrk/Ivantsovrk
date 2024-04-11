06cd74687dcb15b0b0783c125132a2b5132e3f26
Ivantsovrk <ivantsovoleg07@gmail.com>
2024-04-11 14:10:49 +0300

    Changed dependencies

---
+++ utility_luv5_108801_1s9a.ts
@@ -0,0 +1,4 @@
+const deepClone = obj => JSON.parse(JSON.stringify(obj));
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function addNumbers(a, b) { return a + b; }
